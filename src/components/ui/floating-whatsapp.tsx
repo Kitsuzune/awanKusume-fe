b7903de43ec7bacbbf888bcc776f38d78d02699e
Kitsuzune <teguhwmn189@gmail.com>
2024-09-09 16:09:40 +0700

    change wa

--- src/components/ui/floating-whatsapp.tsx
+++ src/components/ui/floating-whatsapp.tsx
@@ -5,7 +5,7 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp';
 export default function FloatingWhatsAppClient() {
   return (
     <FloatingWhatsApp
-      phoneNumber="6282120079861"
+      phoneNumber="628158968885"
       accountName="CS Awan Kusuma"
       avatar="https://png.pngtree.com/png-vector/20191129/ourmid/pngtree-headphone-icon-customer-service-icon-png-image_2047538.jpg"
       statusMessage="Online"
